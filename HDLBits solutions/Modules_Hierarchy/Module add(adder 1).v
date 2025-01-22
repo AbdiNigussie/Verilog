761422e9d88c4c71234626b92168b38b9f970dda
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-23 01:56:03 +0800

    folder

---
+++ HDLBits solutions/Modules_Hierarchy/Module add(adder 1).v
@@ -0,0 +1,22 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cin;
+    wire [15:0] sum1, sum2;
+    add16 A1(
+        .a(a[15:0]),
+        .b(b[15:0]),
+        .cin(0),
+        .sum(sum1),
+        .cout(cin)
+    );
+    add16 A2(
+        .a(a[31:16]),
+        .b(b[31:16]),
+        .sum(sum2),
+        .cin(cin)             
+    );
+    assign sum = {sum2, sum1};
+endmodule
