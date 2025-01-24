634f9325b9d929553921add93fbb2dd41b8a533a
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-24 23:01:44 +0800

    Create If Statements latches.v

---
+++ HDLBits solutions/Procedures/If Statements latches.v
@@ -0,0 +1,21 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated) begin 
+            shut_off_computer = 1; end
+        else shut_off_computer =0;
+    end
+
+    always @(*) begin
+        if (arrived | gas_tank_empty) begin 
+           keep_driving = 0; end
+        else keep_driving =1;
+    end
+
+endmodule
