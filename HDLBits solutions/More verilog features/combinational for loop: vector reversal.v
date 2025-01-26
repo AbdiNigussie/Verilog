874577e64f1c04d2f7b7d7b404d4a7516b12f951
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-26 20:53:29 +0800

    Create combinational for loop: vector reversal.v

---
+++ HDLBits solutions/More verilog features/combinational for loop: vector reversal.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    int i;
+    always@(*)begin 
+        for(i=0; i<=99; i = i+1) begin
+            out[99-i] = in[i];
+        end
+    end
+endmodule
