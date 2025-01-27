28ce1428741902e0449e5f01199c0b1c70f5e25f
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-27 23:37:57 +0800

    Create Generate foor loop: 100 bIit adder.v

---
+++ HDLBits solutions/More verilog features/Generate foor loop: 100 bIit adder.v
@@ -0,0 +1,25 @@
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    assign {cout[0], sum[0]} = a[0] + b[0] + cin;
+    genvar i;
+    generate  
+        for(i=1; i<=99; i=i+1) begin:gen             
+                full_adder (
+                    .in_a(a[i]),
+                    .in_b(b[i]),
+                    .c_in(cout[i-1]),
+                    .c_out(cout[i]),
+                    .sum(sum[i])
+                );            
+        end:gen    
+    endgenerate
+endmodule
+module full_adder(
+	input in_a, in_b,c_in,
+    output c_out,
+    output sum_1 );
+    assign	{c_out, sum_1}= in_a + in_b + c_in;
+endmodule
