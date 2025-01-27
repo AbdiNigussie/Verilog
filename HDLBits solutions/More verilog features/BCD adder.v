90f2c6cb2a57450dd5f1e2e7e868fe98c6c1df9d
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-27 23:50:15 +0800

    Create BCD adder.v

---
+++ HDLBits solutions/More verilog features/BCD adder.v
@@ -0,0 +1,16 @@
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+    wire [99:0]carry;
+    bcd_fadd bcd1(a[3:0], b[3:0], cin, carry[0], sum[3:0]);
+    genvar i;
+    generate begin
+        for(i=4; i<400; i=i+4)begin:gen
+            bcd_fadd bcd1(a[i+3:i], b[i+3:i], carry[(i/4)-1], carry[i/4], sum[i+3:i]);
+        end:gen
+      end
+    endgenerate
+    assign cout=carry[99];
+endmodule
