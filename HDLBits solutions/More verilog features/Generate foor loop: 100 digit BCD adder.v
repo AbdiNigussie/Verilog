d9e532dc338b4759f6fa0711a07cc3af76f563a3
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-27 23:54:30 +0800

    Update Generate foor loop: 100 digit BCD adder.v

--- HDLBits solutions/More verilog features/Generate foor loop: 100 digit BCD adder.v
+++ HDLBits solutions/More verilog features/Generate foor loop: 100 digit BCD adder.v
@@ -2,15 +2,18 @@ module top_module(
     input [399:0] a, b,
     input cin,
     output cout,
-    output [399:0] sum );
-    wire [99:0]carry;
-    bcd_fadd bcd1(a[3:0], b[3:0], cin, carry[0], sum[3:0]);
+    output [399:0] sum 
+);
+    wire [99:0] carry;
+    bcd_fadd bcd_1(a[3:0], b[3:0], cin, carry[0], sum[3:0]); 
+    
     genvar i;
     generate begin
-        for(i=4; i<400; i=i+4)begin:gen
+        for(i = 4; i < 400; i = i + 4) begin:gen
             bcd_fadd bcd1(a[i+3:i], b[i+3:i], carry[(i/4)-1], carry[i/4], sum[i+3:i]);
         end:gen
       end
     endgenerate
+    
     assign cout=carry[99];
 endmodule
