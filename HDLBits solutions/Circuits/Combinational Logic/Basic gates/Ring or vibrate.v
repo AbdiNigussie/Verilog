b919c3ff1aefefd51a6b21be2faec1f1291adb8c
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-30 23:51:27 +0800

    Update Ring or vibrate.v

--- HDLBits solutions/Circuits/Combinational Logic/Basic gates/Ring or vibrate.v
+++ HDLBits solutions/Circuits/Combinational Logic/Basic gates/Ring or vibrate.v
@@ -4,7 +4,7 @@ module top_module (
     output ringer,       // Make sound
     output motor         // Vibrate
 );
-    // simplify the circuit using truth table
+    // simplify the circuit using truth table or oyou can use K-map to simplify
     assign ringer = ring && ~vibrate_mode;
     assign motor = ring && vibrate_mode;
 
