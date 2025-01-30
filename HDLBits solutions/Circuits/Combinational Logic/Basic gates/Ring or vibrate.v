caa50e9e5cd6c4c20a01905e34049cfe5f6447b8
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-01-30 23:42:27 +0800

    Create Ring or vibrate.v

---
+++ HDLBits solutions/Circuits/Combinational Logic/Basic gates/Ring or vibrate.v
@@ -0,0 +1,11 @@
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+    // simplify the circuit using truth table
+    assign ringer = ring && ~vibrate_mode;
+    assign motor = ring && vibrate_mode;
+
+endmodule
