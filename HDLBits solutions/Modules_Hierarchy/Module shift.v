7a6c5fcb098d3efd591afebc52b99f397e99d3d8
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-02-03 00:50:29 +0800

    Update Module shift.v

--- HDLBits solutions/Modules_Hierarchy/Module shift.v
+++ HDLBits solutions/Modules_Hierarchy/Module shift.v
@@ -1,6 +1,6 @@
 module top_module ( input clk, input d, output q );
     wire q1, q2;
-    my_dff d1(
+    my_dff d1(     // instantiate the module and name it d1
         .clk(clk),
         .d(d),
         .q(q1)
