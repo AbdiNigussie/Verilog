641441fd49afd5f5f64deab4a5e59fab8f344591
AbdiNigussie <133047955+AbdiNigussie@users.noreply.github.com>
2025-02-10 23:34:19 +0800

    Update 4 variable with don't care .v

--- HDLBits solutions/Circuits/Combinational Logic/Karnaugh map to circuit/4 variable with don't care .v
+++ HDLBits solutions/Circuits/Combinational Logic/Karnaugh map to circuit/4 variable with don't care .v
@@ -4,7 +4,8 @@ module top_module(
     input c,
     input d,
     output out  ); 
-  
+    // the given K map is different from conventional k_map. check the order of ab and cd.
+
     assign out = a | (~b & c);
 
 endmodule
